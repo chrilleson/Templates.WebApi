f214e889eb4a8131e2a9087df79c87fdb0ecb7da
Christoffer Ljungqvist <Christoffer.Ljungqvist@hotmail.se>
2024-04-13 10:46:15 +0200

    Fixed conditionals in infrastructure

--- src/ApplicationName-Api/ApplicationName.Infrastructure/Persistence/PersistenceExtensions.cs
+++ src/ApplicationName-Api/ApplicationName.Infrastructure/Persistence/PersistenceExtensions.cs
@@ -1,14 +1,19 @@
-﻿#if ef
+﻿#if (ef)
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
-#elif (dapper && sqlServer)
+#elif (dapper)
+    #if (sqlServer && postgres)
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.DependencyInjection;
-using System.Data;
-#elif (dapper && postgres)
 using Npgsql;
+    #elif (sqlServer)
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.DependencyInjection;
+    #elif (postgres)
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+    #endif
 using System.Data;
 #else
 using Microsoft.Extensions.DependencyInjection;
@@ -18,53 +23,59 @@ namespace ApplicationName.Infrastructure.Persistence;
 
 public static class PersistenceExtensions
 {
-#if (ef && sqlServer)
+#if (ef)
     public static void AddPersistence(this IServiceCollection services, string connectionString)
     {
         services.AddDbContext<AppDbContext>(builder => builder.ConfigureDbContext(connectionString));
     }
-
+    #if (sqlServer && postgres)
+    private static void ConfigureDbContext(this DbContextOptionsBuilder builder, string connectionString) =>
+         builder
+             .UseSqlServer(connectionString)
+             .UseNpgsql(connectionString)
+             .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+             .ConfigureWarnings(x => x.Throw(RelationalEventId.MultipleCollectionIncludeWarning));
+    #elif (sqlServer)
     private static void ConfigureDbContext(this DbContextOptionsBuilder builder, string connectionString) =>
          builder
              .UseSqlServer(connectionString)
              .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
              .ConfigureWarnings(x => x.Throw(RelationalEventId.MultipleCollectionIncludeWarning));
-#elif (ef && postgres)
-     public static void AddPersistence(this IServiceCollection services, string connectionString)
-     {
-         services.AddDbContext<AppDbContext>(builder => builder.ConfigureDbContext(connectionString));
-     }
-
+    #elif (postgres)
     private static void ConfigureDbContext(this DbContextOptionsBuilder builder, string connectionString) =>
-        builder
-            .UseNpgsql(connectionString)
-            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
-            .ConfigureWarnings(x => x.Throw(RelationalEventId.MultipleCollectionIncludeWarning));
-#elif (ef)
-     public static void AddPersistence(this IServiceCollection services, string connectionString)
-     {
-         services.AddDbContext<AppDbContext>(builder => builder.ConfigureDbContext(connectionString));
-     }
-
+         builder
+             .UseNpgsql(connectionString)
+             .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+             .ConfigureWarnings(x => x.Throw(RelationalEventId.MultipleCollectionIncludeWarning));
+    #else
     private static void ConfigureDbContext(this DbContextOptionsBuilder builder) =>
-        builder
-            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
-            .ConfigureWarnings(x => x.Throw(RelationalEventId.MultipleCollectionIncludeWarning));
-#elif (dapper && sqlServer)
+         builder
+             .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+             .ConfigureWarnings(x => x.Throw(RelationalEventId.MultipleCollectionIncludeWarning));
+    #endif
+#elif (dapper)
+    #if (sqlServer && postgres)
+    public static void AddPersistence(this IServiceCollection services, string connectionString)
+    {
+        services.AddTransient<IDbConnection>(_ => new SqlConnection(connectionString));
+        services.AddTransient<IDbConnection>(_ => new NpgsqlConnection(connectionString));
+    }
+    #elif (sqlServer)
     public static void AddPersistence(this IServiceCollection services, string connectionString)
     {
         services.AddTransient<IDbConnection>(_ => new SqlConnection(connectionString));
     }
-#elif (dapper && postgres)
+    #elif (postgres)
     public static void AddPersistence(this IServiceCollection services, string connectionString)
     {
         services.AddTransient<IDbConnection>(_ => new NpgsqlConnection(connectionString));
     }
-#elif (dapper)
+    #else
     public static void AddPersistence(this IServiceCollection services, string connectionString)
     {
         services.AddTransient<IDbConnection>(_ => /*Your DB Connection here*/);
     }
+    #endif
 #else
     public static void AddPersistence(this IServiceCollection services)
     {
